ddb9b33aebb0b35110d9685f9114e1925a978383
killla <killa.granet@gmail.com>
2021-11-07 16:16:52 +0500

    Move data from template to view

--- places/models.py
+++ places/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Place(models.Model):
     title = models.CharField('Название', max_length=200)
     description_short = models.TextField('Короткое описание')
--- places/views.py
+++ places/views.py
@@ -2,9 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Place, Image
+
+
+def serialize_point(place):
+    return {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [place.lng, place.lat]
+        },
+        "properties": {
+            "title": place.title,
+            "placeId": place.pk,
+            "detailsUrl": "some.json"
+        }
+    }
+
 
 def main_page(request):
     template = loader.get_template('index.html')
-    context = {}
+    places = Place.objects.all()
+
+    points = {
+        "type": "FeatureCollection",
+        "features": [
+            serialize_point(place) for place in places
+        ]
+    }
+
+    context = {"points": points}
     rendered_page = template.render(context, request)
+
     return HttpResponse(rendered_page)
