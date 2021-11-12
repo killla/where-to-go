164408b1f9befcd7d30bbad367c8a0d4b6d8e8fd
killla <killa.granet@gmail.com>
2021-11-12 19:42:35 +0500

    Fix models

--- places/models.py
+++ places/models.py
@@ -4,18 +4,18 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField('Название', max_length=200)
-    description_short = models.TextField('Короткое описание')
-    description_long = HTMLField('Длинное описание')
+    description_short = models.TextField('Короткое описание', blank=True)
+    description_long = HTMLField('Длинное описание', blank=True)
     lng = models.FloatField('Долгота')
     lat = models.FloatField('Широта')
 
     def __str__(self):
-        return f'{self.title}'
+        return self.title
 
 
 class Image(models.Model):
-    number = models.PositiveSmallIntegerField(default=0)
-    photo = models.ImageField()
+    number = models.PositiveSmallIntegerField(verbose_name='Порядковый номер', blank=True, default=0)
+    photo = models.ImageField(verbose_name='Файл')
     place = models.ForeignKey('Place', on_delete=models.CASCADE, verbose_name='Место', related_name='imgs')
 
     class Meta:
